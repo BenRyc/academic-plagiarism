52110d5e9ed6cb0d622aa9d9603c4cc91b3c279d
Fish4203 <26777965+Fish4203@users.noreply.github.com>
2022-09-06 17:21:09 +1000

    idk bit i think it works

--- Room.py
+++ Room.py
@@ -1,8 +1,10 @@
 
 class Room:
     def __init__(self, x1, z1, x2, z2, y, palette, doors, decor):
-        self.corner1 = (x1, z1)
-        self.corner2 = (x2, z2)
+        self.x1 = x1
+        self.z1 = z1
+        self.x2 = x2
+        self.z2 = z2
         self.y = y
 
         self.palette = palette
