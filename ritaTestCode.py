0d59203c94ab474cd6281e09f643289b938b5c87
Rita <s3947881@student.rmit.edu.au>
2022-09-12 14:23:33 +1000

    added test code for decor, moved room decorator from room.py to decor.py

--- ritaTestCode.py
+++ ritaTestCode.py
@@ -1,6 +1,7 @@
 from mcpi import minecraft
 from mcpi import block
 import Palettes 
+import Decor
 mc = minecraft.Minecraft.create()
 
 playerPos = mc.player.getPos()
@@ -9,10 +10,17 @@ x,y,z = playerPos.x, playerPos.y, playerPos.z
 
 mc.postToChat(mc.getBlockWithData(playerPos.x, playerPos.y-1, playerPos.z))
 
+
+#-----Test Palette-----
 #newPalette = Palettes.housePalette()
 
 #newPalette.pickPalette()
 #mc.postToChat(newPalette.returnPalette())
 
+
+#-----Test Decor-----
+newDecor = Decor([253,95,-17], [255,97,-15], [254,95,-15], 'bedroom')
+
+newDecor.roomDecoratorInator()
 
 
