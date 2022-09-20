6c08705232eb68819dd102bb6c67dd47f088dbbf
Fish4203 <26777965+Fish4203@users.noreply.github.com>
2022-09-20 14:35:46 +1000

    removed palet from room object

--- Room.py
+++ Room.py
@@ -1,13 +1,12 @@
 
 class Room:
-    def __init__(self, x1, z1, x2, z2, y, palette, doors, decor):
+    def __init__(self, x1, z1, x2, z2, y, doors, decor):
         self.x1 = x1
         self.z1 = z1
         self.x2 = x2
         self.z2 = z2
         self.y = y
 
-        self.palette = palette
 
         self.doors = doors
         self.decor = decor
