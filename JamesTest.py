8a1070fc755176e5bea368579331d165bd04000a
JamesLeeRMIT <s3944203@student.rmit.edu.au>
2022-09-25 21:15:29 +1000

    Terraforming now blacklists blocks, defaults to grass

--- JamesTest.py
+++ JamesTest.py
@@ -1,29 +1,28 @@
 from mcpi.minecraft import Minecraft
 from mcpi import block
 
+x = 10
+z = 10
+blocksAvoid = [8, 9, 10, 11, 18, 31, 32, 37, 38, 39, 40, 78]
+
 mc = Minecraft.create()
 
 origin = mc.player.getPos()
 
-rangeX = 100
-rangeZ = 100
-
-scan = []
-blockNum = 0
-
-for Zcoord in range(rangeZ):
-    scanZ = origin.z + ((rangeZ * (-1)) + Zcoord)
-    row = []
-    
-    for Xcoord in range(rangeX):
-        scanX = origin.x + ((rangeX * (-1)) + Xcoord)
-        Y = mc.getHeight(scanX, scanZ)
-        row.append((int(scanX), int(Y), int(scanZ)))
-        blockNum += 1
-        print(str(blockNum) + "/" + str(rangeX*rangeZ))
+for zCoord in range(z):
+    for xCoord in range(x):
+        x = round(xCoord+origin.x)
+        y = mc.getHeight(xCoord+origin.x, zCoord+origin.z)
+        z = round(zCoord+origin.z)
+        valid = True
+        blockScan = mc.getBlockWithData(x, y, z)
         
-    scan.append(row)
-    
-print(scan)
+        for scan in blocksAvoid:
+            scannedBlock = blockScan.id
+            if scannedBlock == scan:
+                valid = False
+                
+        if valid == False:
+            mc.setBlock(x, y, z, 0)
         
         
